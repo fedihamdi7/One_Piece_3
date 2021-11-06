384ef1c07fa36614db2c3471e7b3aa918cb4dd33
eyabenaouicha <eyabenaouicha25@gmail.com>
2021-11-06 23:19:42 +0100

    HOME ROUTING

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthComponent } from './auth/auth.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     AuthComponent,
+    HomeComponent,
 
   ],
   imports: [
