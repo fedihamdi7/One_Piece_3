18b677dff9aefcb3cd4590168956ddf762e2fc62
molka-attia <81327438+molka-attia@users.noreply.github.com>
2021-11-14 16:26:41 +0100

    adding dashboard admin

---
+++ FrontEnd/src/app/admin/admin.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { AdminComponent } from './admin.component';
+import { UserComponent } from './user/user.component';
+
+
+@NgModule({
+  declarations: [
+    AdminComponent,
+    UserComponent
+  ],
+  imports: [
+    CommonModule,
+    AdminRoutingModule
+  ]
+})
+export class AdminModule { }
