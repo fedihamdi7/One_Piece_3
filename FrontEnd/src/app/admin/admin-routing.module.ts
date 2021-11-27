852e011203ca54af1ea6f2dc4cec5794713fc504
molka-attia <81327438+molka-attia@users.noreply.github.com>
2021-11-27 22:53:47 +0100

    adding user and user info

--- FrontEnd/src/app/admin/admin-routing.module.ts
+++ FrontEnd/src/app/admin/admin-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { UserComponent } from './user/user.component';
+import { UserdetailComponent } from './user/userdetail/userdetail.component';
 
-const routes: Routes = [{ path: '', component: AdminComponent },
-{ path:'user', component:UserComponent}];
+const routes: Routes = [
+{ path: '', component: AdminComponent },
+{ path:'user', component:UserComponent},
+{ path:'user/:id', component:UserdetailComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
