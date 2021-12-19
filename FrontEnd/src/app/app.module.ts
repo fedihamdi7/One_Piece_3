65c8a4659011c30b5a9e8dd9452c9b5ea8a2a848
Fedi Hamdi <fedihamdi97@gmail.com>
2021-12-19 13:54:47 +0100

    adding login/signup forms

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthComponent } from './auth/auth.component';
-import { HomeComponent } from './home/home.component';
-import { DashRespoComponent } from './dash-respo/dash-respo.component';
+
 import { ClubComponent } from './club/club.component';
 import { DashAdminComponent } from './dash-admin/dash-admin.component';
 import { AllClubsComponent } from './all-clubs/all-clubs.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { AllClubsComponent } from './all-clubs/all-clubs.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FrontEnd/src/app/auth/auth.component.ts
+++ FrontEnd/src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
+  name:string;
+  email:string;
+  password:string;
+
+  @ViewChild('signin') singInFrom:NgForm;
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onSigninSubmit(){
+    // console.log(this.email,this.password);
+    console.log(this.singInFrom);
+
+  }
+
+  onSignUpSubmit(){
+    console.log(this.name,this.email,this.password);
+  }
+
+
+  // animation
+  @ViewChild('signUp') signUpButton:ElementRef;
+  @ViewChild('signIn') signInButton:ElementRef;
+  @ViewChild('container') container:ElementRef;
+
+ ngAfterViewInit(){
+   this.signUpButton.nativeElement.addEventListener('click',()=>{
+     this.container.nativeElement.classList.add('right-panel-active');
+   })
+   this.signInButton.nativeElement.addEventListener('click',()=>{
+    this.container.nativeElement.classList.remove('right-panel-active');
+  })
+ }
 
 }
