1f3369af2e34fe6b2f6b528fcb7dc8b14691a4d2
Fedi Hamdi <fedihamdi97@gmail.com>
2021-12-22 14:23:19 +0100

    polishing app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/OnePiece',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
+const userRouter = require('./routes/user');
+
+//connecting to database
+mongoose.connect('mongodb://localhost:27017/OnePiece', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log('Connected successfully to MongoDB !'))
   .catch(() => console.log('Connection failed to MongoDB !'));
 
-  app.use(express.json());
+app.use(bodyParser.json());
 
-  app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
 
-app.use('/api/courses', courseRouter);
+// CORS Middleware
+app.use(cors());
+
 app.use('/api/auth', userRouter);
 
 module.exports = app;
