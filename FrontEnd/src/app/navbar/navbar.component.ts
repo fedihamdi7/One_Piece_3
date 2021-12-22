d70ba12521adab55a93c7a6b64b745cd47b53c61
Fedi Hamdi <fedihamdi97@gmail.com>
2021-12-22 19:52:45 +0100

    nav change by status

--- FrontEnd/src/app/navbar/navbar.component.ts
+++ FrontEnd/src/app/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FlashMessagesService } from 'angular2-flash-messages';
+import { Subscription } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -8,15 +8,21 @@ import { AuthService } from '../services/auth.service';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
-
+export class NavbarComponent implements OnInit ,OnDestroy {
+  private authListenerSubs : Subscription;
+  userIsAuthenticated = false;
   constructor(
     public authService : AuthService,
-    private router: Router ,
     private flashMessageService : FlashMessagesService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+      this.authListenerSubs= this.authService.getAuthStatusListener().subscribe(isAuthenticated=>{
+        this.userIsAuthenticated= isAuthenticated;
+      });
+    }
+    ngOnDestroy(): void {
+      this.authListenerSubs.unsubscribe();
+    }
   onLogoutClick() {
     this.authService.logout();
     this.flashMessageService.show('You are logged out', {
