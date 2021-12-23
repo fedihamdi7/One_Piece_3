d12ecd80ca70152d23cf5b326925129fde45ba90
molka-attia <81327438+molka-attia@users.noreply.github.com>
2021-12-23 17:11:51 +0100

    correcting user

--- FrontEnd/src/app/admin/user/user.component.ts
+++ FrontEnd/src/app/admin/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from './user.model';
+import { users } from './users-list';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -7,23 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
   public username="molka"
-  public users = [
-    {
-      id:"1",
-      name: 'John Doe',
-      user_img:"image",
-      email: 'john.doe.gmail.com',
-      type:'admin',
-    
-    },
-    {
-      id:"2",
-      name: 'John Doe2',
-      user_img:"image2",
-      email: 'john.doe.gmail.com2',
-      type:'admin',
-    },
-  ];
+  public usersList : User[] = users;
   constructor() { }
 
 
