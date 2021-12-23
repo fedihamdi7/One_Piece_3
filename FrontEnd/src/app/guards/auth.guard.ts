990a8bfd344d7349ef7b3b390ffca39084b1a33a
Fedi Hamdi <fedihamdi97@gmail.com>
2021-12-23 23:04:07 +0100

    fix guard

--- FrontEnd/src/app/guards/auth.guard.ts
+++ FrontEnd/src/app/guards/auth.guard.ts
@@ -5,11 +5,19 @@ import { AuthService } from "../services/auth.service";
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
-
+  userIsAuthenticated = false;
   canActivate(): boolean {
-    if (this.authService.getAuthStatusListener()) {
+    const token = localStorage.getItem('id_token');
+      if(token){
+        this.userIsAuthenticated=true;
+      }
+    if (this.userIsAuthenticated) {
+      console.log("guard true");
+
       return true;
     } else {
+      console.log("guard false");
+
       this.router.navigate(['/auth']);
       return false;
     }
