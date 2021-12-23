c609e5992f24fab0536471f10adf1a70cfe78784
eyabenaouicha <eyabenaouicha25@gmail.com>
2021-12-23 12:50:11 +0100

    ff

---
+++ FrontEnd/src/app/home/home-list.ts
@@ -0,0 +1,37 @@
+import { Event, Home } from "./home.model";
+export const homes: Event[] = [
+   {
+      event_date: "2019-01-01",
+      event_img :"4c(3).jpg"
+    },
+    {
+      event_date: "2020-05-13",
+      event_img :"mcncom(1).jpg"
+    },
+    {
+      event_date: "2021-11-18",
+      event_img :"tunivisions(1).jpg"
+    }]
+//   ],
+//   club:[{
+//     "id":"1",
+//     "name": 'IEEE',
+//     "club_img":"ieee.jpeg",
+//     "about":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda, at aperiam, dolorem consequuntur cupiditate, debitis facere dolore harum non natus quae saepe eveniet mollitia eligendi ullam labore velit reprehenderit vel.",
+
+//   },
+//   {
+//     "id":"2",
+//     "name": 'tunivisions',
+//     "club_img":"ieee.jpeg",
+//     "about":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda, at aperiam, dolorem consequuntur cupiditate, debitis facere dolore harum non natus quae saepe eveniet mollitia eligendi ullam labore velit reprehenderit vel.",
+
+//   },{
+//     "id":"3",
+//     "name": 'IEEE',
+//     "club_img":"ieee.jpeg",
+//     "about":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda, at aperiam, dolorem consequuntur cupiditate, debitis facere dolore harum non natus quae saepe eveniet mollitia eligendi ullam labore velit reprehenderit vel.",
+//   }]
+
+// }
+// ];
--- FrontEnd/src/app/home/home.component.ts
+++ FrontEnd/src/app/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Event, Home } from './home.model';
+import { homes } from './home-list';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public events : Event[] = homes;
+
+  // public mokka= homes;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.events);
+
   }
 
 }
