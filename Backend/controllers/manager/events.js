4d705cf5913eef87aacbe2902e353e322c29c10f
Fedi Hamdi <fedihamdi97@gmail.com>
2021-12-26 16:47:13 +0100

    event stats backend function

--- Backend/controllers/manager/events.js
+++ Backend/controllers/manager/events.js
@@ -7,7 +7,6 @@ exports.get = (req, res, next) => {
 }
 
 exports.update = (req, res, next) => {
-    console.log(req.body);
     club.updateOne({'events.event_id':req.params.id},
     {'$set':{
         'events.$.event_date':req.body.event_date,
@@ -33,4 +32,12 @@ exports.delete = (req, res, next) => {
 exports.getLogo = (req, res, next) => {
     club.find({'_id':req.params.id},{'image':1,'_id':0})
     .then(image => res.json(image));
+}
+
+exports.getStats = (req, res, next) => {
+    club.aggregate([
+        {$match:{_id :mongoose.Types.ObjectId(req.params.id)}},
+        {$project : {"events" : {$size :"$events"},_id:0}}
+        ])
+        .then(stats => res.json(stats[0]));
 }
